d47f94fee4026669a673b60ec995f74e72c777a7
Meron Brouwer <meron.brouwer@gmail.com>
2018-05-08 09:38:29 +0200

    Fixed all unittests

---
+++ src/test/java/nl/han/ica/OOPDProcessingEngineHAN/Tile/MockTile.java
@@ -0,0 +1,12 @@
+package nl.han.ica.OOPDProcessingEngineHAN.Tile;
+
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.Sprite;
+
+public class MockTile extends Tile{
+    /**
+     * @param sprite
+     */
+    public MockTile(Sprite sprite) {
+        super(sprite);
+    }
+}
