3300af51e01345ddd5184e9330da907fe69b6861
Eline Koch <eline.koch@hotmail.com>
2021-04-23 11:21:33 +0200

    initial commit, includes some practice with the OOGP Game Engine

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"TutorialWorld","l":"Disc"},{"p":"TutorialWorld","l":"Player"},{"p":"TutorialWorld","l":"TutorialWorld"}];updateSearchResults();
---
+++ src/main/java/TutorialWorld/tiles/FloorTile.java
@@ -0,0 +1,14 @@
+package TutorialWorld.tiles;
+
+import nl.han.ica.oopg.objects.Sprite;
+import nl.han.ica.oopg.tile.Tile;
+
+public class FloorTile extends Tile {
+	/**
+	 * 
+	 * @param sprite image to be drawn as a floor Tile.
+	 */
+	public FloorTile(Sprite sprite) {
+		super(sprite);
+	}
+}
